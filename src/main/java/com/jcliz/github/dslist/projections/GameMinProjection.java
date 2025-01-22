85cb720373521629c41f4c83f406accb5f8c112c
Jcliz <joaopedrocliz@gmail.com>
2025-01-22 10:12:21 -0300

    Homologação concluída

--- src/main/java/com/jcliz/github/dslist/projections/GameMinProjection.java
+++ src/main/java/com/jcliz/github/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.jcliz.github.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
