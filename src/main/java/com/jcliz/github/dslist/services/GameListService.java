b3ed4f62e180cf2100830ab90514381205990231
Jcliz <joaopedrocliz@gmail.com>
2025-01-23 10:13:00 -0300

    Aplicação da lógica de mudança de posição na lista

--- src/main/java/com/jcliz/github/dslist/services/GameListService.java
+++ src/main/java/com/jcliz/github/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.jcliz.github.dslist.services;
 
 import com.jcliz.github.dslist.dto.GameListDTO;
 import com.jcliz.github.dslist.entities.GameList;
+import com.jcliz.github.dslist.projections.GameMinProjection;
 import com.jcliz.github.dslist.repositories.GameListRepository;
+import com.jcliz.github.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +16,29 @@ public class GameListService {
 
     @Autowired
     private GameListRepository gameListRepository;
+    @Autowired
+    private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         //tabela de games se torna uma lista
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    //garante que tudo execute
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
